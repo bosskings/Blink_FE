77e6173c90fd3dc01f4e079e443176b11de300ba
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2025-10-08 03:34:58 -0700

    first push

---
+++ app/(noaccess)/_layout.tsx
@@ -0,0 +1,45 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { router, Stack } from 'expo-router';
+import React, { useEffect } from 'react';
+
+const StackPagesLayout = () => {
+  useEffect(() => {
+    (async () => {
+      try {
+        const onboarded = await AsyncStorage.getItem("blink_onboarding");
+        const token = await AsyncStorage.getItem("blink_token");
+        if (token) {
+          router.replace("/home");
+          return;
+        }
+
+        if (onboarded) {
+          router.replace("/login");
+          return;
+        } else {
+          router.replace("/onboarding");
+          return;
+        }
+  
+      } catch (error) {
+        console.error(error);
+        router.replace("/login");
+        return;
+      }
+    })();
+      return () => {};
+    }, []);
+  return (
+    <Stack
+      screenOptions={{
+      headerShown: false,
+    }}
+    >
+      <Stack.Screen name="index" />
+      <Stack.Screen name="onboarding" />
+      <Stack.Screen name="login" />
+    </Stack>
+  )
+}
+
+export default StackPagesLayout
